d7b88f13f6828a6077953a31d0ad3673a023694a
nikhilesh <nikhilesh273@gmail.com>
2026-03-19 17:31:39 +0530

    feat: add Vite build configuration with base path and output directory.

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  // If you are deploying to username.github.io/HEARTENZA-SERVICES/
+  // set 'base' to '/HEARTENZA-SERVICES/'
+  // If you are using a custom domain or username.github.io, set to '/'
+  base: './', 
+  build: {
+    outDir: 'dist',
+  }
+});
